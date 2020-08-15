8e6f95d85e49cc08538e76a4fe6774a1047c2ccf
ha quang le <winlp4ever@gmail.com>
2020-08-15 22:24:48 +0200

    insert graphql

--- server.js
+++ server.js
@@ -8,12 +8,34 @@ const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const http = require('http');
 
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
+
 // set up server
 var app = express();
 // app.use(favicon(path.join(__dirname, 'imgs', 'favicon.ico'))); -- uncomment this line if u have a favicon for your site
 app.use(express.static(__dirname + './public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 const prodConfig = require('./webpack.prod.js');
 const devConfig = require('./webpack.dev.js');
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,21 @@
 import React, {useState} from 'react';
 
 const App = (props) => {
+    const handleClick = async () => {
+        let response = await fetch('/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({query: "{ hello }"})
+        })
+        let data = await response.json()
+        console.log(data)
+    }
     return <div className='app'>
         hello world
+        <button onClick={handleClick}>Click me!</button>
     </div>
 }
 
